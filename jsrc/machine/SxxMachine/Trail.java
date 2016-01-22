dc5fa0aefc2d0f8f4ebc636630d8ddde79acc467
Aleksei Semenov <alsemenov@users.noreply.github.com>
2016-01-22 10:10:37 +0000

    PR 14180: Prolog Cafe improvements

--- jsrc/machine/SxxMachine/Trail.java
+++ jsrc/machine/SxxMachine/Trail.java
@@ -1,4 +1,8 @@
 package com.googlecode.prolog_cafe.lang;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Trail stack.<br>
  * The class <code>Trail</code> represents a trail stack.<br>
@@ -10,7 +14,10 @@ package com.googlecode.prolog_cafe.lang;
  * @version 1.0
  */
 public final class Trail {
-    /** An array of <code>Undoable</code> entries. */
+	/** list of already filled buffers */
+    private final Deque<Undoable[]> buffersList;
+
+	/** An array of <code>Undoable</code> entries. */
     private Undoable[] buffer;
 
     /** the top index of this <code>Trail</code>. */
@@ -21,13 +28,14 @@ public final class Trail {
 
     /** Constructs a new trail stack. */
     public Trail() {
-	this(20000);
+	this(2048);
     }
 
     /** Constructs a new trail stack with the given size. */
     public Trail(int n) {
-	buffer = new Undoable[n];
-	top = -1;
+    	buffersList = new ArrayDeque<Undoable[]>(1000);
+    	buffer = new Undoable[n];
+    	top = -1;
     }
 
     /** Discards all entries. */
@@ -35,51 +43,58 @@ public final class Trail {
 
     /** Pushs an entry to this <code>Trail</code>. */
     public void push(Undoable t) {
-	try {
-	    buffer[++top] = t;
-	} catch (ArrayIndexOutOfBoundsException e) {
-	    int len = buffer.length;
-	    Undoable[] new_buffer = new Undoable[len+20000];
-		System.arraycopy(buffer, 0, new_buffer, 0, len);
-	    buffer = new_buffer;
-	    buffer[top] = t;
-	}
+		if (++top >= buffer.length) {
+			buffersList.addLast(buffer);
+			buffer = new Undoable[buffer.length];
+			top = 0;
+		}
+		buffer[top] = t;
     }
 
     /** Pops an entry from this <code>Trail</code>. */
     public Undoable pop() {
-	Undoable t = buffer[top];
-	buffer[top--] = null;
-	return t;
+    	Undoable t = buffer[top];
+    	buffer[top--] = null;
+    	if (top<0 && buffersList.size()>0){
+    		buffer = buffersList.pollLast();
+    		top = buffer.length-1;
+    	}
+    	return t;
     }
 
     /** Discards all entries. */
     protected void deleteAll() {
-	while (! empty()) {
-	    buffer[top--] = null;
-	}	
+    	buffersList.clear();
+    	buffer = new Undoable[buffer.length];
+    	top = -1;
     }
 
     /** Tests if this stack has no entry. */
     public boolean empty() {
-	return top == -1;
+    	return buffersList.isEmpty() && top == -1;
     }
 
     /** Current allocation of the trail storage array.  */
-    public int max() { return buffer.length; }
+    public int max() { return (buffersList.size()+1)*buffer.length; }
 
-    /** Returns the value of <code>top</code>. 
+    /** Returns the value of <code>top</code>.
      * @see #top
      */
-    public int top() { return top; }
+    public int top() { return (buffer.length * buffersList.size()) + top; }
 
     /** Unwinds all entries after the value of <code>i</code>. */
     public void unwind(int i) {
-	Undoable t;
-	while (top > i) {
-	    t = pop();
-	    t.undo();
-	}
+		int base = buffer.length * buffersList.size();
+		while (base + top > i) {
+	    	Undoable t = buffer[top];
+	    	buffer[top--] = null;
+	    	if (top<0 && buffersList.size()>0){
+	    		buffer = buffersList.pollLast();
+	    		top = buffer.length-1;
+	    		base -= buffer.length;
+	    	}
+		    t.undo();
+		}
     }
 
 //    /** Shows the contents of this <code>Trail</code>. */
