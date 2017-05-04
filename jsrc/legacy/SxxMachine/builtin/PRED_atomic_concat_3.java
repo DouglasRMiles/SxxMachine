dd786e9c93ee21b6bde5d999d138422b65038c7d
Aleksei Semenov <alsemenov@users.noreply.github.com>
2017-05-04 09:41:11 +0000

    PR 14270: JPL: engine optimizations optimize logging predicates reduce vpformat usage avoid reflection in vpfindall and evm_call

---
+++ jsrc/legacy/SxxMachine/builtin/PRED_atomic_concat_3.java
@@ -0,0 +1,40 @@
+package com.googlecode.prolog_cafe.builtin;
+
+import com.googlecode.prolog_cafe.lang.*;
+
+/**
+ * atomic_concat(+Atomic1, +Atomic2, -Atom)
+ * Atom represents the text after converting Atomic1 and Atomic2 to text and concatenating the result:
+ *  ?- atomic_concat(name, 42, X).
+ *  X = name42.
+ * Created by semenov on 27.04.2017.
+ * http://www.swi-prolog.org/pldoc/man?predicate=atomic_concat/3
+ */
+public class PRED_atomic_concat_3 extends Predicate.P3 {
+
+	public PRED_atomic_concat_3(Term arg1, Term arg2, Term arg3, Operation cont) {
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		this.arg3 = arg3;
+		this.cont = cont;
+	}
+
+	@Override
+	public Operation exec(Prolog engine) throws PrologException {
+		final Term a1 = arg1.dereference();
+		final Term a2 = arg2.dereference();
+
+		if (!(a1 instanceof NumberTerm) && !(a1 instanceof SymbolTerm)) {
+			throw new IllegalTypeException(this, 1, "atomic", a1);
+		}
+
+		if (!(a2 instanceof NumberTerm) && !(a2 instanceof SymbolTerm)) {
+			throw new IllegalTypeException(this, 2, "atomic", a2);
+		}
+
+		if (arg3.unify(SymbolTerm.create(a1.toString()+a2.toString()), engine.trail)) {
+			return cont;
+		}
+		return engine.fail();
+	}
+}
